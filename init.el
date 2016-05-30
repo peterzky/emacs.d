d5bcd9af224e8cd89e8ecdce85e126ba8e93f214
peterzky <378096232@qq.com>
2016-05-30 13:55:59 +0800

    add window numbering

--- init.el
+++ init.el
@@ -11,6 +11,9 @@
 
 ; List the packages you want
 (setq package-list '(evil
+		     window-numbering
+		     org
+		     org-plus-contrib
                      evil-leader
 		     auto-complete
 		     ranger
@@ -27,6 +30,8 @@
 ; only contains a very limited number of packages
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t) 
+(add-to-list 'package-archives 
+             '("org" . "http://orgmode.org/elpa/") t)
 ; Activate all the packages (in particular autoloads)
 (package-initialize)
 
@@ -42,6 +47,12 @@
 (require 'evil)
 (evil-mode t)
 
+(defun peter/switch-buffer ()
+  (interactive)
+  (if (evil-alternate-buffer)
+      (switch-to-buffer (car (evil-alternate-buffer)))
+    (switch-to-buffer (other-buffer (current-buffer) t))))
+
 (require 'evil-leader)
 (global-evil-leader-mode)
 (evil-leader/set-leader "<SPC>")
@@ -52,7 +63,11 @@
   "ff" 'find-file
   "qq" 'save-buffers-kill-emacs
   "o" 'other-window
-  "1" 'delete-other-windows)
+  "1" 'delete-other-windows
+  "ws" 'split-window-below
+  "wv" 'split-window-right
+  "wd" 'delete-window
+  "TAB" 'peter/switch-buffer)
 
 (require 'auto-complete)
 (ac-config-default)
@@ -75,8 +90,10 @@
 
 (require 'ido-vertical-mode)
 (ido-mode 1)
+(ido-everywhere 1)
 (ido-vertical-mode 1)
 (setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
 
 
 (require 'org-bullets)
@@ -90,3 +107,35 @@
 (require 'smex)
 (global-set-key (kbd "s-x") 'smex)
 (global-set-key (kbd "s-X") 'smex-major-mode-commands)
+
+(require 'window-numbering)
+(window-numbering-mode)
+(evil-leader/set-key
+  "1" 'select-window-1
+  "2" 'select-window-2
+  "3" 'select-window-3
+  "4" 'select-window-4
+  "5" 'select-window-5
+  "6" 'select-window-6
+  "7" 'select-window-7
+  "8" 'select-window-8
+  "9" 'select-window-9
+  "0" 'select-window-0
+  )
+ 
+      
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :foreground "dark cyan" :height 1.3))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1)))))
