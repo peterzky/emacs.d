fc2bfdfcdaccbc04d6271df392f848f489af4ed2
peterzky <378096232@qq.com>
2016-07-07 01:14:55 +0800

    chinese font

--- init.el
+++ init.el
@@ -318,6 +318,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cfs--current-profile "profile1" t)
+ '(cfs--profiles-steps (quote (("profile1" . 3))) t)
  '(inhibit-startup-screen t)
  '(shell-pop-shell-type
    (quote
