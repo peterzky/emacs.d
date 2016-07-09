8f022544c5e0034a5e0f0925b3c5741e8fbb0609
peterzky <378096232@qq.com>
2016-07-10 01:35:04 +0800

    font tinker

--- config/custom.el
+++ config/custom.el
@@ -6,6 +6,9 @@
  '(bmkp-last-as-first-bookmark-file "/home/peterzky/.emacs.d/bookmarks")
  '(cfs--current-profile "profile1" t)
  '(cfs--profiles-steps (quote (("profile1" . 3))) t)
+ '(cfs-disable-bold t)
+ '(cfs-disable-bold-italic t)
+ '(cfs-disable-italic t)
  '(elfeed-feeds
    (quote
     ("http://endlessparentheses.com/atom.xml" "http://www.economist.com/sections/science-technology/rss.xml" "http://survivejs.com/atom.xml" "http://blog.aaronbieber.com/feed.xml" "http://feeds.feedburner.com/SanityInc" "http://planet.emacsen.org/atom.xml" "http://pragmaticemacs.com/feed/" "http://sachachua.com/blog/feed" "http://feeds.feedburner.com/paul-irish" "https://scotch.io/feed" "http://feeds.feedburner.com/AlexSexton" "https://www.smashingmagazine.com/feed/" "http://feeds.feedburner.com/SixRevisions" "https://davidwalsh.name/feed/atom" "http://speckyboy.com/feed/" "http://www.onextrapixel.com/feed/" "http://webresourcesdepot.com/feed/" "http://www.phpied.com/feed/" "http://dailyjs.com/rss/" "http://www.unixmen.com/feed/" "http://www.cyberciti.biz/feed/" "http://feeds.feedburner.com/codinghorror/" "http://feeds.feedburner.com/arkency.xml" "http://blog.andrewray.me/rss/" "http://www.justinweiss.com/atom.xml" "http://devblog.avdi.org/feed/" "http://osxdaily.com/category/tips-tricks/feed/" "http://feeds.feedburner.com/theiphoneblog" "http://www.macstories.net/feed/" "http://9to5mac.com/feed/" "http://rss.macworld.com/macworld/feeds/main" "http://www.cnblogs.com/renly/rss" "http://feeds.feedburner.com/ruanyifeng" "http://wangbixi.com/feed/" "http://mindhacks.cn/feed/" "http://blog.zhaojie.me/rss" "http://feed.williamlong.info/" "http://feeds.feedburner.com/playpc" "http://songshuhui.net/feed" "http://sspai.com/feed" "http://feeds.geekpark.net/" "http://feed.iplaysoft.com/" "http://www.ifanr.com/feed" "http://www.waerfa.com/feed" "http://lucumr.pocoo.org/feed.atom" "http://blog.evernote.com/feed/" "http://blog.atom.io/feed.xml" "http://feeds.feedburner.com/ScottHanselman" "http://feeds2.feedburner.com/Command-line-fu" "http://htedsv.logdown.com/posts.rss" "http://www.tennfy.com/feed" "http://maybeiwill.me/feed/" "http://hansay.com/atom.xml" "http://blog.csdn.net/liuyukuan/rss/list" "http://feeds.feedburner.com/zenhabits" "http://feeds.gawker.com/lifehacker/vip")))
@@ -15,7 +18,7 @@
  '(mm-text-html-renderer (quote w3m))
  '(package-selected-packages
    (quote
-    (youdao-dictionary yasnippet window-numbering w3m use-package smex smartparens shell-pop ranger projectile pdf-tools paradox org-plus-contrib org-bullets js2-mode ido-vertical-mode ido-ubiquitous helm-swoop helm-ag evil-surround evil-nerd-commenter evil-mc evil-magit evil-leader emmet-mode elfeed deft company-tern chinese-pyim-greatdict chinese-pyim chinese-fonts-setup bookmark+ avy auto-compile ample-theme)))
+    (tern youdao-dictionary yasnippet window-numbering w3m use-package smex smartparens shell-pop ranger projectile pdf-tools paradox org-plus-contrib org-bullets js2-mode ido-vertical-mode ido-ubiquitous helm-swoop helm-ag evil-surround evil-nerd-commenter evil-mc evil-magit evil-leader emmet-mode elfeed deft company-tern chinese-pyim-greatdict chinese-pyim chinese-fonts-setup bookmark+ avy auto-compile ample-theme)))
  '(shell-pop-shell-type
    (quote
     ("eshell-pop" "*eshell-pop*"
