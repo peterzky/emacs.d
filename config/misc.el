22e5a1d0129d61415b88232e795ac07acfa00ee3
peterzky <378096232@qq.com>
2016-07-14 22:56:48 +0800

    remove interface config file move to misc.el

--- config/misc.el
+++ config/misc.el
@@ -54,3 +54,34 @@
   (evil-leader/set-key
     "ar" 'deer))
 
+(use-package avy
+  :ensure t
+  :config
+  (evil-leader/set-key
+    "SPC" 'avy-goto-word-0))
+
+(global-set-key (kbd "s-/") 'dabbrev-expand)
+
+
+
+(use-package shell-pop
+  :ensure t
+  :config
+  (global-set-key (kbd "C-`") 'shell-pop)
+  (setq shell-pop-full-span t))
+
+(defalias 'open 'find-file-other-window)
+
+(defun eshell/clear ()
+  "Clear the eshell buffer."
+  (let ((inhibit-read-only t))
+    (erase-buffer)))
+
+(use-package paradox
+  :ensure t
+  :config
+  (setq paradox-github-token t)
+  (evil-set-initial-state 'paradox-menu-mode 'emacs)
+  (evil-leader/set-key
+    "pr" 'paradox-list-packages
+    "pu" 'paradox-upgrade-packages))
