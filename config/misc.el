615d32b195bca57e583f8b808eaca2332e6244fd
peterzky <378096232@qq.com>
2016-07-21 12:37:36 +0800

    add git timemachine

--- config/misc.el
+++ config/misc.el
@@ -18,6 +18,18 @@
   (evil-leader/set-key
     "gs" 'magit-status))
 
+(use-package git-timemachine
+  :ensure t
+  :config
+  (evil-leader/set-key
+    "gm" 'git-timemachine)
+  )
+
+(eval-after-load 'git-timemachine
+  '(progn
+     (evil-make-overriding-map git-timemachine-mode-map 'normal)
+     ;; force update evil keymaps after git-timemachine-mode loaded
+     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))
 
 (use-package projectile
   :ensure t
