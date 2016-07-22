27f2051278233fefda6f85ee377086eb92a5a275
peterzky <378096232@qq.com>
2016-07-22 10:28:04 +0800

    man page in current buffer

--- config/custom.el
+++ config/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method (quote pushy))
  '(cfs--current-profile "profile1" t)
  '(cfs--profiles-steps (quote (("profile1" . 4))) t)
  '(cfs-disable-bold t)
