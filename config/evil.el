2e9de0351a63b87edc8414fced766cc8ce50a14f
peterzky <378096232@qq.com>
2016-07-22 09:44:38 +0800

    unmap key

--- config/evil.el
+++ config/evil.el
@@ -74,6 +74,6 @@
 (use-package evil-numbers
     :ensure t
     :config
-    (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-    (define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+    ;; (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+    ;; (define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
     )
