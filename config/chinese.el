c7cc9392d90d8f1028b32f883832dd2eb03843bf
peterzky <378096232@qq.com>
2016-12-30 12:43:02 +0800

    remove package list from customize

--- config/chinese.el
+++ config/chinese.el
@@ -4,19 +4,19 @@
   (evil-leader/set-key
     "oo" 'youdao-dictionary-search-at-point+))
 
-(use-package chinese-pyim-greatdict
-  :ensure t)
+;; (use-package chinese-pyim-greatdict
+;;   :ensure t)
 
-(use-package chinese-pyim
-  :ensure t
-  :config
-  (chinese-pyim-greatdict-enable)
-  (setq pyim-use-tooltip 'popup)
-  (setq pyim-enable-words-predict nil))
+;; (use-package chinese-pyim
+;;   :ensure t
+;;   :config
+;;   (chinese-pyim-greatdict-enable)
+;;   (setq pyim-use-tooltip 'popup)
+;;   (setq pyim-enable-words-predict nil))
 
-(setq default-input-method "chinese-pyim")
-(global-set-key (kbd "C-\\") 'toggle-input-method)
+;; (setq default-input-method "chinese-pyim")
+;; (global-set-key (kbd "C-\\") 'toggle-input-method)
 
-(use-package chinese-fonts-setup
-  :ensure t
-  :config) 
+;; (use-package chinese-fonts-setup
+;;   :ensure t
+;;   :config) 
