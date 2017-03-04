3d671502a419ab1ca35b438369aeeae792f82aac
peterzky <378096232@qq.com>
2017-03-04 22:16:54 +0800

    fix

--- config/custom.el
+++ config/custom.el
@@ -43,14 +43,9 @@
       :publishing-function org-publish-attachment :recursive t)
      ("web" :components
       ("images" "js" "css")))))
- '(org2jekyll-blog-author "Peterzky" nil (org2jekyll))
- '(org2jekyll-jekyll-directory (expand-file-name "~/Projects/Blog/") nil (org2jekyll))
- '(org2jekyll-jekyll-drafts-dir "" nil (org2jekyll))
- '(org2jekyll-jekyll-posts-dir "_posts/" nil (org2jekyll))
- '(org2jekyll-source-directory (expand-file-name "~/org/") nil (org2jekyll))
  '(package-selected-packages
    (quote
-    (htmlize org2jekyll evil-mu4e shm hindent gorepl-mode company-ghc ghc info+ flycheck-gometalinter systemd youdao-dictionary yasnippet window-numbering web-beautify use-package spaceline smartparens shell-pop scion rainbow-mode pp-c-l popwin paradox org-pomodoro org-plus-contrib org-bullets json-mode js2-mode js-comint hungry-delete helm-systemd helm-projectile helm-descbinds helm-ag haskell-mode graphviz-dot-mode go-eldoc git-timemachine fcitx expand-region evil-surround evil-numbers evil-nerd-commenter evil-mc evil-magit evil-leader emmet-mode deft crux company-tern company-go chinese-pyim-greatdict chinese-pyim avy auto-compile ample-theme)))
+    (helm tern projectile htmlize org2jekyll evil-mu4e shm hindent gorepl-mode company-ghc ghc info+ flycheck-gometalinter systemd youdao-dictionary yasnippet window-numbering web-beautify use-package spaceline smartparens shell-pop scion rainbow-mode pp-c-l popwin paradox org-pomodoro org-plus-contrib org-bullets json-mode js2-mode js-comint hungry-delete helm-systemd helm-projectile helm-descbinds helm-ag haskell-mode graphviz-dot-mode go-eldoc git-timemachine fcitx expand-region evil-surround evil-numbers evil-nerd-commenter evil-mc evil-magit evil-leader emmet-mode deft crux company-tern company-go chinese-pyim-greatdict chinese-pyim avy auto-compile ample-theme)))
  '(shell-pop-shell-type
    (quote
     ("eshell-pop" "*eshell-pop*"
@@ -63,18 +58,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bold ((t (:foreground "firebrick" :weight bold))))
- '(evil-mc-cursor-default-face ((t (:background "blue violet" :inverse-video nil))))
- '(info-quoted-name ((t (:inherit font-lock-string-face :foreground "DarkOrange2"))))
- '(info-single-quote ((t (:inherit font-lock-keyword-face :foreground "dark violet"))))
- '(italic ((t (:foreground "peru" :slant italic))))
- '(org-block ((t (:foreground "#bdbdb3"))))
- '(org-code ((t (:inherit shadow :foreground "dark orange"))))
- '(org-level-1 ((t (:inherit outline-1 :foreground "dark cyan" :height 1.1))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
- '(org-pomodoro-mode-line ((t (:foreground "dark red"))))
- '(sp-pair-overlay-face ((t nil)))
- '(spaceline-evil-emacs ((t (:background "#3A539B" :foreground "#D2D7D3" :inherit (quote mode-line)))))
- '(spaceline-evil-insert ((t (:background "YellowGreen" :foreground "#3E3D31" :inherit (quote mode-line)))))
- '(spaceline-evil-motion ((t (:background "#663399" :foreground "#D2D7D3" :inherit (quote mode-line))))))
+ )
--- config/org.el
+++ config/org.el
@@ -71,62 +71,71 @@
 
 (add-hook 'org-agenda-mode-hook 'peter/agenda-mode-config)
 
-;; Org to Jekyll
-(use-package org2jekyll
+;; Org code block color html
+(use-package htmlize
   :ensure t
   :config)
 
-(custom-set-variables
- '(org2jekyll-blog-author "Peterzky")
- '(org2jekyll-source-directory (expand-file-name "~/org/"))
- '(org2jekyll-jekyll-directory (expand-file-name "~/Projects/Blog/"))
- '(org2jekyll-jekyll-drafts-dir "")
- '(org2jekyll-jekyll-posts-dir "_posts/")
- '(org-publish-project-alist
-   `(("default"
-      :base-directory ,(org2jekyll-input-directory)
-      :base-extension "org"
-      :publishing-directory ,(org2jekyll-output-directory)
-      :publishing-function org-html-publish-to-html
-      :headline-levels 4
-      :section-numbers nil
-      :with-toc nil
-      :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
-      :html-preamble t
-      :recursive t
-      :make-index t
-      :html-extension "html"
-      :body-only t)
-     ("post"
-      :base-directory ,(org2jekyll-input-directory)
-      :base-extension "org"
-      :publishing-directory ,(org2jekyll-output-directory org2jekyll-jekyll-posts-dir)
-      :publishing-function org-html-publish-to-html
-      :headline-levels 4
-      :section-numbers nil
-      :with-toc nil
-      :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
-      :html-preamble t
-      :recursive t
-      :make-index t
-      :html-extension "html"
-      :body-only t)
-     ("images"
-      :base-directory ,(org2jekyll-input-directory "img")
-      :base-extension "jpg\\|gif\\|png"
-      :publishing-directory ,(org2jekyll-output-directory "img")
-      :publishing-function org-publish-attachment
-      :recursive t)
-     ("js"
-      :base-directory ,(org2jekyll-input-directory "js")
-      :base-extension "js"
-      :publishing-directory ,(org2jekyll-output-directory "js")
-      :publishing-function org-publish-attachment
-      :recursive t)
-     ("css"
-      :base-directory ,(org2jekyll-input-directory "css")
-      :base-extension "css\\|el"
-      :publishing-directory ,(org2jekyll-output-directory "css")
-      :publishing-function org-publish-attachment
-      :recursive t)
-     ("web" :components ("images" "js" "css")))))
+
+
+
+
+;; ;; Org to Jekyll
+;; (use-package org2jekyll
+;;   :ensure t
+;;   :config)
+
+;; (custom-set-variables
+;;  '(org2jekyll-blog-author "Peterzky")
+;;  '(org2jekyll-source-directory (expand-file-name "~/org/"))
+;;  '(org2jekyll-jekyll-directory (expand-file-name "~/Projects/Blog/"))
+;;  '(org2jekyll-jekyll-drafts-dir "")
+;;  '(org2jekyll-jekyll-posts-dir "_posts/")
+;;  '(org-publish-project-alist
+;;    `(("default"
+;;       :base-directory ,(org2jekyll-input-directory)
+;;       :base-extension "org"
+;;       :publishing-directory ,(org2jekyll-output-directory)
+;;       :publishing-function org-html-publish-to-html
+;;       :headline-levels 4
+;;       :section-numbers nil
+;;       :with-toc nil
+;;       :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
+;;       :html-preamble t
+;;       :recursive t
+;;       :make-index t
+;;       :html-extension "html"
+;;       :body-only t)
+;;      ("post"
+;;       :base-directory ,(org2jekyll-input-directory)
+;;       :base-extension "org"
+;;       :publishing-directory ,(org2jekyll-output-directory org2jekyll-jekyll-posts-dir)
+;;       :publishing-function org-html-publish-to-html
+;;       :headline-levels 4
+;;       :section-numbers nil
+;;       :with-toc nil
+;;       :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
+;;       :html-preamble t
+;;       :recursive t
+;;       :make-index t
+;;       :html-extension "html"
+;;       :body-only t)
+;;      ("images"
+;;       :base-directory ,(org2jekyll-input-directory "img")
+;;       :base-extension "jpg\\|gif\\|png"
+;;       :publishing-directory ,(org2jekyll-output-directory "img")
+;;       :publishing-function org-publish-attachment
+;;       :recursive t)
+;;      ("js"
+;;       :base-directory ,(org2jekyll-input-directory "js")
+;;       :base-extension "js"
+;;       :publishing-directory ,(org2jekyll-output-directory "js")
+;;       :publishing-function org-publish-attachment
+;;       :recursive t)
+;;      ("css"
+;;       :base-directory ,(org2jekyll-input-directory "css")
+;;       :base-extension "css\\|el"
+;;       :publishing-directory ,(org2jekyll-output-directory "css")
+;;       :publishing-function org-publish-attachment
+;;       :recursive t)
+;;      ("web" :components ("images" "js" "css")))))
