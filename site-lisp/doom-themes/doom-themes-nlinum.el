c5f4f2b0435c7487154e1806ccd5985b07788bcf
peterzky <peter.zky@qq.com>
2018-07-09 20:15:06 +0800

    remove doom-theme from site-lisp

--- site-lisp/doom-themes/doom-themes-nlinum.el
+++
@@ -1,6 +0,0 @@
-;;; doom-themes-nlinum.el
-
-(message "doom-themes: nlinum support is gone. nlinum 1.7+ has the nlinum-highlight-current-line option." )
-
-(provide 'doom-themes-nlinum)
-;;; doom-themes-nlinum.el ends here
